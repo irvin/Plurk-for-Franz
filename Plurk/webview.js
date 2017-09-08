19df55bdf28d91e7b86c13f256ff2085b2d35077
Irvin <irvinfly@gmail.com>
2017-09-09 02:07:45 +0800

    support unread badge (not really working for direct mention)

--- Plurk/webview.js
+++ Plurk/webview.js
@@ -1,3 +1,8 @@
 module.exports = (Franz, options) => {
-    
+  function getMessages() {
+    count = document.querySelector('#noti_count').textContent * 1;
+    count_indirect = document.querySelector('#noti_re_count').textContent * 1;
+    Franz.setBadge(count, count_indirect);
+  }
+  Franz.loop(getMessages);
 }
