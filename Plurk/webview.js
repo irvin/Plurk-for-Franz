05d3af3c6d9087f224d1d29171a9fca603b2a6ab
Irvin <irvinfly@gmail.com>
2017-11-26 03:56:15 +0800

    update syntax for Franz 5

--- Plurk/webview.js
+++ Plurk/webview.js
@@ -1,8 +1,8 @@
 module.exports = (Franz, options) => {
   function getMessages() {
-    count = document.querySelector('#noti_count').textContent * 1;
-    count_indirect = document.querySelector('#noti_re_count').textContent * 1;
+    let count = document.querySelector('#noti_count').textContent * 1;
+    let count_indirect = document.querySelector('#noti_re_count').textContent * 1;
     Franz.setBadge(count, count_indirect);
   }
   Franz.loop(getMessages);
-}
+};
